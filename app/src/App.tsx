50e69abf69bdf379b2f4c4178e66320e67fac8cf
Trevor Huis in 't Veld <trevorhuis@bithuis.com>
2024-01-03 20:37:59 -0600

    add app

---
+++ app/src/App.tsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <div className="mx-auto max-w-7xl sm:px-6 lg:px-8">
+      <h1 className="text-3xl text-center font-bold text-gray-900">
+        trevorhuisintveld.dev
+      </h1>
+    </div>
+  );
+}
